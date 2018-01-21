b8b307e87683b6926eb545f0a4473171bf8bfccd
rikrik527 <rik6827@gmail.com>
2018-01-21 11:48:18 +0800

    updated register form source code and reposition yushan and add newly created image background still updating

--- src/form/index.js
+++ src/form/index.js
@@ -1,42 +1,76 @@
 require('../sass/form.scss');
+window.onload = function() {
+        var link = "https://code.jquery.com/jquery-3.3.0.js";
+        var integrity = 'integrity="sha256-TFWSuDJt6kS+huV+vVlyV1jM3dwGdeNWqezhTxXB/X8="';
+        var cross = 'crossorigin="anonymous"';
+        var script = document.createElement('script');
+        script.src = link + ' ' + integrity + ' ' + cross;
+        document.body.appendChild(script);
 
-(function() {
-    document.forms.register.noValidate = true;
 
-    var form = document.getElementsByTagName('form')[0];
-    form.addEventListener('submit', function(e) {
-        var elements = this.elements;
-        var valid = {};
-        var isValid;
-        var isFormValid;
-        for (var i = 0, l = elements.length - 1; i < l; i++) {
-            isValid = validateRequired(elements[i]) && validateType(elements[i]);
-            if (!isValid) {
-                showErrorMessage(elements[i]);
+
+    }
+    (function() {
+
+        document.querySelector('.form-boxcontrol').noValidate = true;
+
+        var form = document.getElementsByTagName('form')[0];
+        form.addEventListener('submit', function(e) {
+            var elements = this.elements;
+            var valid = {};
+            var isValid;
+            var isFormValid;
+            for (var i = 0, l = elements.length - 1; i < l; i++) {
+                isValid = validateRequired(elements[i]) && validateType(elements[i]);
+                if (!isValid) {
+                    showErrorMessage(elements[i]);
+                } else {
+                    removeErrorMessage(elements[i]);
+                }
+                valid[elements[i].id] = isValid;
+            }
+            if (!validateBio()) {
+                showErrorMessage(document.getElementById('bio'));
+                valid.bio = false;
             } else {
-                removeErrorMessage(elements[i]);
+                removeErrorMessage(document.getElementById('bio'));
             }
-            valid[elements[i].id] = isValid;
-        }
-        if (!validateBio()) {
-            showErrorMessage(document.getElementById('bio'));
-            valid.bio = false;
-        } else {
-            removeErrorMessage(document.getElementById('bio'));
-        }
-        for (var field in valid) {
-            if (!valid[field]) {
-                isFormValid = false;
-                break;
+            for (var field in valid) {
+                if (!valid[field]) {
+                    isFormValid = false;
+                    break;
+                }
+                isFormValid = true;
             }
-            isFormValid = true;
-        }
-        if (!isFormValid) {
-            e.preventDefault();
-        }
-        // functions will be excute code here
-    })
-})();
+            if (!isFormValid) {
+                e.preventDefault();
+            }
+            // functions will be excute code here
+            function validateRequired(el) {
+                if (isRequired(el)) {
+                    var valid = !isEmpty(el);
+                    if (!valid) {
+                        setErrorMessage(el, 'Field is required');
+                    }
+                    return valid;
+                }
+                return true;
+            }
+
+            function isRequired(el) {
+                return ((typeof el.required === 'boolean') && el.required) ||
+                    (typeof el.required === 'string');
+            }
+
+            function isEmpty(el) {
+                return !el.value || el.value === el.placeholder;
+            }
+
+            function setErrorMessage(el, message) {
+                $(el).data('errorMessage', message);
+            }
+        })
+    })();
 
 
 document.querySelector('#name').onfocus = function() {
