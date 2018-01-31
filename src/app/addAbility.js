b72a53f804709559c697137560b82d6b9b43fe2c
rikrik527 <rik6827@gmail.com>
2018-01-31 12:28:30 +0800

    added parts now click parts icon the value will increase by 1 each time and press submit to add values to progress part

--- src/app/addAbility.js
+++ src/app/addAbility.js
@@ -13,36 +13,53 @@ var object = require('./object');
 // }
 module.exports.parts = function() {
     document.querySelector('.todo').addEventListener('click', addParts, false);
+    document.querySelector('#todo-submit-btn').addEventListener('click', submitParts)
+    document.querySelector('#todo-reset-btn').addEventListener('click', resetParts);
+    var add = 0;
+    var parts = [];
+    var total;
+    var todoparts = document.querySelector('.todo-parts');
 
     function addParts() {
-        var todoparts = document.querySelector('.todo-parts');
-        var add = 0;
-        var parts = [];
-        return function() {
-            add++;
-            var total = parts.push(add);
-            console.log(add, total);
-            todoparts.value += total;
-            if (document.querySelector('#todo-submit-btn').onsubmit) {
-                todoparts.value += total;
+        console.log('add')
 
-                console.log(total);
-            } else if (document.querySelector('#todo-reset-btn').onreset) {
-                parts = [];
-                console.log(parts);
-            }
+
+
+        console.log('todoparts returning')
+        add++;
+        total = parts.push(add);
+        console.log(add, total);
+        if (add < 5) {
+            document.querySelector('.todo-set').textContent = '準備為雨珊加' + add + '個零件!';
+        } else {
+            document.querySelector('.todo-set').textContent = '加油!繼續增加中!準備為雨珊加' + add + '個零件!';
         }
+
     };
 
+    function submitParts() {
+
+        console.log('submit', parts, add, total);
+        todoparts.value += add;
+        parts = [];
+
+        add = 0;
+        document.querySelector('.todo-set').textContent = '已經為雨珊加了' + total + '個零件!';
+    }
+
+    function resetParts() {
+        console.log('reset', parts, add, total)
+        parts = [];
+        add = 0;
+
+        document.querySelector('.todo-set').textContent = '已取消';
+    }
     var todo = document.querySelector('.todo');
     todo.onmouseover = function() {
         console.log('mouseover')
-        document.querySelector('.todo-set').textContent = 'add parts for shan li';
+        document.querySelector('.todo-set').textContent = '為雨珊加點零件吧!';
         var todoparts = document.querySelector('.todo-parts');
-        var sumbit = document.querySelector('#todo-submit-btn').setAttribute('parts', true);
+
     }
-    todo.onmouseout = function() {
-        console.log('mouseout');
-        document.querySelector('.todo-set').textContent = '';
-    }
+
 }
