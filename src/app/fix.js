9c29275b6aa9959af4d9a55efc7d8b848e4d5f06
rikrik527 <rik6827@gmail.com>
2018-02-14 21:50:45 +0800

    add function fix button to fix shan li on html and css later on will do canvas version and webgl verstion

--- src/app/fix.js
+++ src/app/fix.js
@@ -14,15 +14,21 @@ module.exports.fixMenu = function() {
         robotSaying = obj.get('.robot-saying'),
         robotSpeak = obj.get('.robot-speak'),
         robotTriangle = obj.get('.robot-triangle'),
-        robotEnergyBar = obj.get('.robot-energy-bar');
+        robotEnergyBar = obj.get('.robot-energy-bar'),
+        gameActionOutline = obj.get('.game-action-outline'),
+        gameAction = obj.get('.game-action');
+    iconPersonal.style.left = '0px';
+    iconService.style.left = '0px';
+    iconFix.style.left = '0px';
+    iconFix.style.zIndex = '-9';
+    iconPersonal.style.zIndex = '-9';
+    iconService.style.zIndex = '-9';
     iconTodo.onclick = (function() {
         var click = 0;
 
         return function() {
             console.log('i have been excuted');
-            iconPersonal.style.left = '0px';
-            iconService.style.left = '0px';
-            iconFix.style.left = '0px';
+
             click++;
             console.log(click)
             if (click == 3) click = 1
@@ -32,7 +38,7 @@ module.exports.fixMenu = function() {
                     iconTodo.style.boxShadow = ' 0px -3px 52px 5px yellow';
                     setTimeout(function() {
                         iconTodo.style.boxShadow = '0px 0px 0px 0px transparent';
-                    }, 50);
+                    }, 500);
                     setTimeout(function() {
                         iconPersonal.style.left = '64px';
                         iconService.style.left = '64px';
@@ -45,24 +51,34 @@ module.exports.fixMenu = function() {
                     setTimeout(function() {
                         iconFix.style.left = '180px';
                     }, 1000);
+                    iconFix.style.zIndex = '999';
+                    iconPersonal.style.zIndex = '9999';
+                    iconService.style.zIndex = '99999';
                     break;
                 case 2:
                     iconTodo.style.boxShadow = ' 0px -3px 52px 5px yellow';
                     setTimeout(function() {
                         iconTodo.style.boxShadow = '0px 0px 0px 0px transparent';
-                    }, 50);
+                    }, 500);
                     setTimeout(function() {
                         iconFix.style.left = '120px';
+
+                        console.log('0')
                     }, 0);
                     setTimeout(function() {
                         iconService.style.left = '64px';
                         iconFix.style.left = '64px';
+                        console.log('500')
                     }, 500);
                     setTimeout(function() {
                         iconFix.style.left = '0px';
                         iconPersonal.style.left = '0px';
                         iconService.style.left = '0px';
+                        console.log('1000')
                     }, 1000);
+                    iconFix.style.zIndex = '-9';
+                    iconPersonal.style.zIndex = '-9';
+                    iconService.style.zIndex = '-9';
                     break;
             }
 
@@ -75,6 +91,31 @@ module.exports.fixMenu = function() {
         robotSpeak.textContent = '';
     }
 
+    iconFix.onclick = function() {
+        iconFix.style.boxShadow = '0px 0px 52px 3px yellow';
+        robotOutLine.classList.remove('robot-outline-fly-back');
+        setTimeout(function() {
+            iconFix.style.boxShadow = '0px 0px 0px 0px transparent';
 
+        }, 100);
+        robot.classList.add('robot-fly');
+        setTimeout(function() {
+            robotOutLine.classList.add('robot-outline-fly');
+        }, 1000);
+
+        setTimeout(function() {
+            robot.classList.remove('robot-fly');
+            robot.classList.add('robot-fix-1');
+        }, 3000);
+        setTimeout(function() {
+            gameAction.classList.add('heart-explode');
+        }, 4000);
+        setTimeout(function() {
+            robotOutLine.classList.remove('robot-outline-fly');
+            robotOutLine.classList.add('robot-outline-fly-back');
+            robot.classList.remove('robot-fix-1');
+            gameAction.classList.remove('heart-explode');
+        }, 10000);
+    };
 
 }
